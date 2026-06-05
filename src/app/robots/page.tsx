c0d6c3f8d43b920fa61c29378ca450103affe93c
Vaish-72 <pradhanvaishnavi25@gmail.com>
2026-06-05 17:46:28 +0530

    style: add overflow-x-hidden to homepage and robots page wrappers to prevent horizontal scrolling

--- src/app/robots/page.tsx
+++ src/app/robots/page.tsx
@@ -5,7 +5,7 @@ import ReviewMarquee from "@/components/ReviewMarquee";
 
 export default function RobotsPage() {
   return (
-    <main className="relative bg-[#050816] text-white">
+    <main className="relative bg-[#050816] text-white overflow-x-hidden">
       <Navbar />
       {/* Cockpit: full viewport, no internal scroll */}
       <RobotShowcase />
